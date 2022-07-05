e674c303385486db0cfaea757bdd4369ea0b295c
Shirish Mainali <shirishmainali2@gmail.com>
2022-07-05 17:39:46 +0545

    :tada:begin with string concatenation

---
+++ basic/string.py
@@ -0,0 +1,17 @@
+print(r"this is \\ double backlash")
+print(9/3)#fps
+print(9//3)#integer division
+print(9*3)#mul
+print(9**3)#power
+print(round(2**3.14,4))
+
+# -->multiline comment
+#emoji
+#print("U+1F600")#replace + with threee zeros 000
+print("\U0001F600")
+
+#stringconcatenation and int to string conversion
+
+print("shirish"+" "+"mainali")
+print("shirish"+"mainali"+str(123)) 
+
