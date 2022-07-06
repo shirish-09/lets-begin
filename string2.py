8dc5c3e15169c44e0028ef109a71e8c566b1d916
Shirish Mainali <shirishmainali2@gmail.com>
2022-07-06 17:05:39 +0545

    :hammar:shirish

---
+++ string2.py
@@ -0,0 +1,5 @@
+#string formatting
+
+first_name,second_name=input("enter you full name(space separated):").split(" ")
+name_type=type(first_name)
+print("your full name is:"+first_name+" "+second_name)
